5fa6ff675cd0fbff54215c0c2b16cc334fb2c42b
DimaSurdiy <dsurdiy@ukr.net>
2023-07-18 05:40:13 +0300

    added js, updated styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(function(){
 
-
+  $(".mavic").hover(
+    function() {
+      $(".mavic-img").addClass("mavic-on").removeClass("mavic-off");
+      $(".mavic").addClass("animation-on").removeClass("animation-off");
+    },
+    function() {
+      $(".mavic-img").removeClass("mavic-on").addClass("mavic-off");
+      $(".mavic").removeClass("animation-on").addClass("animation-off");
+    }
+  );
 
 
 });
